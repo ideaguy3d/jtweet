c0c92cd52b586fd5fde908d310d8bf1c6889677a
Julius Alvarado <angularjs.uiux@gmail.com>
2017-04-05 00:22:53 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 4/2/2017
+ * Time: 6:53 PM
+ */
+
+echo "Ello there ^_^/";
---
+++ views/header.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Julius Alvarado
+ * Date: 4/5/2017
+ * Time: 12:18 AM
+ */
