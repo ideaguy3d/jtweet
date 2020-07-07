69a2a21674a2feb210618df4b16e15248f4eaaed
julius <phpninja@mail.com>
2020-07-06 17:53:35 -0700

    completed my first multiple component react widget

--- public/app/components/basic/task-app.js
+++ public/app/components/basic/task-app.js
@@ -3,7 +3,26 @@
 let TaskApp = React.createClass({
 
     getInitialState: function () {
-        return {items: ['Study React', 'Study Laravel']}
+        return {
+            items: ['Study React', 'Study Laravel'],
+            task: ''
+        };
+    },
+
+    onChange: function (eventObj) {
+        let task = eventObj.target.value;
+        // eventObj.target is the input element
+        this.setState({task});
+    },
+
+    addTask: function (eventObj) {
+        eventObj.preventDefault();
+        
+        //this.state.task += eventObj.target.value;
+        this.setState({
+            items: this.state.items.concat([this.state.task]),
+            task: ''
+        });
     },
 
     render: function () {
@@ -11,6 +30,11 @@ let TaskApp = React.createClass({
             <div>
                 <h1>Tasks for day</h1>
                 <TaskList items={this.state.items}/>
+
+                <form onSubmit={this.addTask}>
+                    <input type="text" onChange={this.onChange} value={this.state.task}/>
+                    <button>add task</button>
+                </form>
             </div>
         );
     }
