ad03c78693c95c6f73c04e8c4ab6b590d44d6dfa
julius <phpninja@mail.com>
2020-07-07 20:21:54 -0700

    a practice stopwatch react component

--- public/app/components/basic/stopwatch.js
+++ public/app/components/basic/stopwatch.js
@@ -3,17 +3,65 @@
 let Stopwatch = React.createClass({
 
     getInitialState: function () {
-      return {
-          time: 0
-      }
+        return {
+            time: 0,
+            until: 0,
+            enabled: true
+        };
+    },
+
+    start: function (e) {
+        e.preventDefault();
+
+        // React.findDOMNode(this.refs.button).disabled = true;
+        this.setState({enabled: false});
+
+        this.interval = setInterval(function () {
+            this.tick();
+            if (this.isTimeUp()) {
+                this.finish();
+            }
+        }.bind(this), 1000);
+    },
+
+    type: function (e) {
+        this.setState({until: e.target.value});
+    },
+
+    isTimeUp: function () {
+        return this.state.time == this.state.until;
+    },
+
+    finish: function () {
+        console.log('buzzzzzzzzz');
+
+        //this.setState({time: 0, until: '', enabled: true});
+        this.setState(this.getInitialState());
+
+        React.findDOMNode(this.refs.input).focus();
+
+        // React.findDOMNode(this.refs.button).disabled = false;
+
+        return clearInterval(this.interval);
+    },
+
+    tick: function () {
+        this.setState({time: this.state.time + 1});
     },
 
     render: function () {
         return (
-            <div>
-                <h1></h1>
-            </div>
+            <form onSubmit={this.start}>
+                <h1>stopwatch = {this.state.time}</h1>
+                <p>{this.state.until ? 'will count up to: ' + this.state.until : ''}</p>
+                <input ref="input" type="text"
+                       onChange={this.type} value={this.state.until}/>
+
+                <button disabled={!this.state.enabled}>go</button>
+            </form>
         );
     }
 
 });
+
+React.render(<Stopwatch/>, document.querySelector('#stopwatch'));
