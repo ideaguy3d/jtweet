9ace7d0bfec2ee5d99e8eb24bc5735e3a59b9033
julius <phpninja@mail.com>
2020-07-12 14:56:52 -0700

    fillable prop, some css, renamed web routes

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
 */
 
 Route::get('/', function() {
@@ -36,10 +35,11 @@ Route::get('/about', function() {
     return view('about', ['articles' => $articles]);
 });
 
+
 Route::get('/articles', 'ArticlesController@index');
 Route::post('/articles', 'ArticlesController@store');
 Route::get('/articles/create', 'ArticlesController@create');
 // wildcards go after
-Route::get('/articles/{id}', 'ArticlesController@show');
-Route::get('/articles/{id}/edit', 'ArticlesController@edit');
-Route::put('/articles/{id}', 'ArticlesController@update');
+Route::get('/articles/{article}', 'ArticlesController@show');
+Route::get('/articles/{article}/edit', 'ArticlesController@edit');
+Route::put('/articles/{article}', 'ArticlesController@update');
